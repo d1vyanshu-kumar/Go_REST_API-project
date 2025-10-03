cd0fe49a18225de5fa1c500a793ce08b8ef79c75
divyanshu_Kumar <154233802+d1vyanshu-kumar@users.noreply.github.com>
2025-10-03 13:38:54 +0530

    inital commit

---
+++ cmd/Students-api/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	// This is just a placeholder main function.
+	fmt.Println("Students API service is starting...")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/d1vyanshu-kumar/students-api
+
+go 1.25.1
